4caa36db1b75bd276e122d68d7b7c4be3ef72558
slawoe <info@slawomirernst.de>
2020-09-07 16:03:48 +0200

    Change server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const jsonServer = require("json-server");
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "client/build")));
-
 app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
 );
+app.use(middlewares);
+app.use(router);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
