8a40a4d05e9fc5db3770b99b86ab40955655dd3b
slawoe <info@slawomirernst.de>
2020-09-07 15:51:57 +0200

    Add api script

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const jsonServer = require("json-server");
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-
 const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "client/build")));
-app.use(middlewares);
-app.use(router);
+
 app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
