368429b30b1aba709775636ebb2d6d0b2d532549
slawoe <info@slawomirernst.de>
2020-09-08 10:28:01 +0200

    Add import code to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
   return (
