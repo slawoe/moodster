ab2402571220dd4e427524ba8c9f4d565ce9c134
slawoe <info@slawomirernst.de>
2020-09-09 12:16:51 +0200

    Add font and change basic GlobalStyles

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -11,11 +11,21 @@ const GlobalCSS = () => {
         }
         #root {
           height: 100vh;
+          width: 100vw;
+          margin: 0px;
+          @import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
         }
         html {
         }
         body {
-          background-color: pink;
+          background-color: #4da4bc;
+          font-family: "Silom";
+
+          @font-face {
+            font-family: "Silom";
+            src: local("Silom"),
+              url(./src/assets/Fonts/SilomBol.ttf) format("ttf");
+          }
         }
         code {
         }
