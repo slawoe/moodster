a4832ecd452c666f8188e914d29f93c8455528aa
slawoe <info@slawomirernst.de>
2020-09-09 11:23:44 +0200

    Start cleaning cra

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 // import {
 //   BrowserRouter as Router,
 //   Switch,
@@ -9,24 +7,7 @@ import "./App.css";
 // } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
