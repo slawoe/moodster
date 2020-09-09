ae3546cf90688bdea3156584169de1618d953abe
slawoe <info@slawomirernst.de>
2020-09-09 13:46:25 +0200

    Add Silom font

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Global, css } from "@emotion/core";
+
 const GlobalCSS = () => {
   return (
     <Global
@@ -11,11 +12,29 @@ const GlobalCSS = () => {
         }
         #root {
           height: 100vh;
+          width: 100vw;
+          margin: 0px;
+          @import url("//db.onlinewebfonts.com/c/784670a6bb0f0b4c713488e8f162a30a?family=Silom");
+          @font-face {
+            font-family: "Silom";
+            src: url("//db.onlinewebfonts.com/t/784670a6bb0f0b4c713488e8f162a30a.eot");
+            src: url("//db.onlinewebfonts.com/t/784670a6bb0f0b4c713488e8f162a30a.eot?#iefix")
+                format("embedded-opentype"),
+              url("//db.onlinewebfonts.com/t/784670a6bb0f0b4c713488e8f162a30a.woff2")
+                format("woff2"),
+              url("//db.onlinewebfonts.com/t/784670a6bb0f0b4c713488e8f162a30a.woff")
+                format("woff"),
+              url("//db.onlinewebfonts.com/t/784670a6bb0f0b4c713488e8f162a30a.ttf")
+                format("truetype"),
+              url("//db.onlinewebfonts.com/t/784670a6bb0f0b4c713488e8f162a30a.svg#Silom")
+                format("svg");
+          }
         }
         html {
         }
         body {
-          background-color: pink;
+          background-color: #4da4bc;
+          font-family: "Silom";
         }
         code {
         }
