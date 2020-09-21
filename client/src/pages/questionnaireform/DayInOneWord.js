cae28ed332d1b0b9c6df5031a3a9d44c58bb5117
slawoe <info@slawomirernst.de>
2020-09-21 13:00:28 +0200

    Add DayInOneWord.js

---
+++ client/src/pages/questionnaireform/DayInOneWord.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import InputField from "../../components/InputField";
+import GoBackButton from "../../components/GoBackButton";
+import NextButton from "../../components/NextButton";
+import FormPageWrapper from "../../components/FormPageWrapper";
+import BasicTextCard from "../../components/BasicTextCard";
+import { Link } from "react-router-dom";
+
+function DayInOneWord({ setForm, formData }) {
+  const { dayInOneWord } = formData;
+  return (
+    <FormPageWrapper>
+      <div>
+        <BasicTextCard>
+          <p>
+            Ok, das habe ich mir aufgeschrieben. Lass uns weitermachen, jetzt
+            darfst Du kreativer werden. Beschreib mir deinen Tag in einem Wort.
+          </p>
+        </BasicTextCard>
+      </div>
+      <div>
+        <InputField
+          label="Dein Wort:"
+          name="dayInOneWord"
+          value={dayInOneWord}
+          placeholder="Mein Wort"
+          onChange={setForm}
+        />
+      </div>
+      <div>
+        <Link to="/main/questionnaire/">
+          <GoBackButton />
+        </Link>
+        <Link to="/main/questionnaire/whatWasGood">
+          <NextButton />
+        </Link>
+      </div>
+    </FormPageWrapper>
+  );
+}
+
+export default DayInOneWord;
+
+DayInOneWord.propTypes = {
+  setForm: PropTypes.any.isRequired,
+  formData: PropTypes.any.isRequired,
+};
