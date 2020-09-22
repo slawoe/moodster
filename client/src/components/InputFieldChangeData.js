dfca10c5e1207c46d4feff64ab6eee0b2918d384
slawoe <info@slawomirernst.de>
2020-09-22 11:38:58 +0200

    Add minor component for Change doctors and medics pages

---
+++ client/src/components/InputFieldChangeData.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+const InputFieldWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  & > label {
+    background: transparent;
+    border: none;
+    border-radius: 5px;
+    padding: 0.4em 0.7em;
+  }
+  & > input {
+    border: none;
+    border-radius: 5px;
+    box-shadow: var(--main-elements-shadow);
+    padding: 0.4em 0.7em;
+    font-family: "Silom";
+    color: var(--inputfield-text-color);
+    align-self: flex-end;
+    width: 50%;
+  }
+`;
+
+function InputField({ label, type = "text", ...otherProps }) {
+  return (
+    <InputFieldWrapper>
+      <label>{label}</label>
+      <input type={type} {...otherProps} />
+    </InputFieldWrapper>
+  );
+}
+
+export default InputField;
+
+InputField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+};
