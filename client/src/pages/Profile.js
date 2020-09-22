4ec06deb46d64e3129904ee3479b0773b3436618
slawoe <info@slawomirernst.de>
2020-09-22 14:15:58 +0200

    change profile pages to folder and setup a Switch in Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,55 +1,21 @@
 import React from "react";
-import StyledTextContainer from "../components/StyledTextContainer";
-import BasicPageLayout from "../components/BasicPageLayout";
-import InfoElementWrapper from "../components/InfoElementWrapper";
-import { Link } from "react-router-dom";
-
-const mockupData = {
-  firstName: "Max",
-  lastName: "Mustermann",
-  nickName: "Maxi",
-  birthDay: "01.01.2010",
-  userName: "maxi.muster",
-  moodstername: "Moody",
-};
+import { Switch, Route, useRouteMatch } from "react-router-dom";
+import ProfileView from "./profile/ProfileView";
+import ProfileChange from "./profile/ProfileChange";
 
 function Profile() {
+  const { path } = useRouteMatch();
   return (
-    <BasicPageLayout
-      childrenheadsection={<></>}
-      childrenmainsection={
-        <>
-          <StyledTextContainer>
-            <h1>Dein Profil</h1>
-            <InfoElementWrapper>
-              <span>Vorname:</span>
-              <span>{mockupData.firstName}</span>
-            </InfoElementWrapper>
-            <InfoElementWrapper>
-              <span>Nachname:</span>
-              <span>{mockupData.lastName}</span>
-            </InfoElementWrapper>
-            <InfoElementWrapper>
-              <span>Spitzname:</span>
-              <span>{mockupData.nickName}</span>
-            </InfoElementWrapper>
-            <InfoElementWrapper>
-              <span>Geburtsdatum:</span>
-              <span>{mockupData.birthDay}</span>
-            </InfoElementWrapper>
-            <InfoElementWrapper>
-              <span>Benutzername:</span>
-              <span>{mockupData.userName}</span>
-            </InfoElementWrapper>
-            <InfoElementWrapper>
-              <span>Dein Moodster heißt:</span>
-              <span>{mockupData.moodstername}</span>
-            </InfoElementWrapper>
-            <Link href="#">Profil bearbeiten...</Link>
-          </StyledTextContainer>
-        </>
-      }
-    />
+    <>
+      <Switch>
+        <Route path={`${path}/change`}>
+          <ProfileChange />
+        </Route>
+        <Route exact path={`${path}/`}>
+          <ProfileView />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
