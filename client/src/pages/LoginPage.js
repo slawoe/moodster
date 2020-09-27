29ef8d0b0c42e6136bcf9d1a60c78e32e888200a
slawoe <info@slawomirernst.de>
2020-09-27 11:40:33 +0200

    Add Login function to LoginPage and change from form to div, also setUp sessionStorage

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import Moodster from "../assets/images/moodster.svg";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { fetchUserWelcome } from "../api/user";
 
 const LoginWrapper = styled.div`
   grid-area: 2 / 2 / 3 / 3;
@@ -11,11 +12,11 @@ const LoginWrapper = styled.div`
   & > h1 {
     font-size: 3em;
   }
-  & > form {
+  & > div {
     display: flex;
     flex-direction: column;
   }
-  & form > input {
+  & div > input {
     border: none;
     border-radius: 5px;
     box-shadow: var(--main-elements-shadow);
@@ -23,14 +24,12 @@ const LoginWrapper = styled.div`
     font-family: "Silom";
     padding: 0.5em 0.5em;
   }
-  & form > span {
+  & div > span {
     padding: 0.4em 0;
     font-size: 1.25em;
   }
-  & form > a {
+  & div > button {
     align-self: center;
-  }
-  & form > a > button {
     background-color: var(--button-main-color);
     border: none;
     border-radius: 5px;
@@ -50,20 +49,59 @@ const LoginWrapper = styled.div`
 `;
 
 function LoginPage() {
+  const history = useHistory();
+  const [userName, setUserName] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [error, setError] = useState(false);
+
+  function userNameChange(userName) {
+    setUserName(userName.target.value);
+  }
+  function passwordChange(password) {
+    setPassword(password.target.value);
+  }
+
+  async function Login() {
+    const newUser = await fetchUserWelcome(userName);
+    if (newUser.length === 0) {
+      setError(true);
+      return;
+    }
+    const parsedUser = newUser[0];
+    if (password !== parsedUser.password) {
+      setError(true);
+    } else {
+      history.push("/main");
+      sessionStorage.userName = parsedUser.userName;
+      sessionStorage.id = parsedUser.id;
+      sessionStorage.nickName = parsedUser.nickName;
+    }
+  }
+
   return (
     <LoginWrapper>
       <h1>MOODSTER</h1>
       <img src={Moodster} alt="Logo" />
-      <form>
-        <input placeholder="max.mustermann"></input>
+      <div>
+        <input
+          name="userName"
+          placeholder="max.mustermann"
+          onChange={userNameChange}
+        />
         <span>Benutzername</span>
-        <input type="password" placeholder="Passwort"></input>
+        <input
+          type="password"
+          name="password"
+          placeholder="Passwort"
+          onChange={passwordChange}
+        />
         <span>Passwort</span>
-        <Link to="/main">
-          <button type="submit">LOGIN</button>
-        </Link>
-      </form>
+        <button type="submit" onClick={Login}>
+          LOGIN
+        </button>
+      </div>
       <Link to="/register">NEU HIER? ANMELDEN...</Link>
+      {error && <p>NUTZERNAME ODER PASSWORT FALSCH!</p>}
     </LoginWrapper>
   );
 }
