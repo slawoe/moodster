bad4b92c1b05025359ab0825a5ff344b7fc6ff7a
slawoe <info@slawomirernst.de>
2020-09-29 11:26:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
 const path = require("path");
-const jsonServer = require("json-server");
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3001;
+const app = express();
+const { initDatabase } = require("./lib/database");
+
+const users = require("./lib/routes/users");
+
+app.use(express.json());
+
+app.use("/api/users", users);
 
 app.use(express.static(path.join(__dirname, "client/build")));
-app.use(
-  "/storybook",
-  express.static(path.join(__dirname, "client/storybook-static"))
-);
-app.use("/api", router);
 
-app.use(middlewares);
-// Handle React routing, return all requests to React app
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
+
+initDatabase(process.env.MONGO_URI, process.env.MONGO_DB).then(async () => {
+  console.log(`Database ${process.env.MONGO_DB} is ready`);
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 });
