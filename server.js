60bb0177b2b7d9f74371ca1777e6b8c9808c591f
slawoe <info@slawomirernst.de>
2020-09-30 17:58:20 +0200

    Update to server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use("/api/medics", medics);
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.use(
