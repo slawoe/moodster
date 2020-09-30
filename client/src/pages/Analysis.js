11482804ecc9218e5ce08af5828baec2fe4115a7
slawoe <info@slawomirernst.de>
2020-09-30 20:21:23 +0200

    Add diary fetch in client and model in toplevel, add functionality to charts page and update ErrorPage with message

--- client/src/pages/Analysis.js
+++ client/src/pages/Analysis.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChartContainer from "../components/ChartContainer";
 import BasicPageLayout from "../components/BasicPageLayout";
 import {
@@ -9,25 +9,50 @@ import {
   YAxis,
   ResponsiveContainer,
 } from "recharts";
+import { fetchLastDiaryEntries } from "../api/diary";
+import Loading from "./LoadingPage";
+import Error from "./ErrorPage";
 
 function Analysis() {
-  const data = [
-    { name: "1", uv: 1, pv: 0, amt: 0 },
-    { name: "2", uv: 3, pv: 0, amt: 0 },
-    { name: "3", uv: 5, pv: 0, amt: 0 },
-    { name: "4", uv: 5, pv: 0, amt: 0 },
-    { name: "5", uv: 6, pv: 0, amt: 0 },
-    { name: "6", uv: 7, pv: 0, amt: 0 },
-    { name: "7", uv: 9, pv: 0, amt: 0 },
-    { name: "8", uv: 9, pv: 0, amt: 0 },
-    { name: "9", uv: 2, pv: 0, amt: 0 },
-    { name: "10", uv: 3, pv: 0, amt: 0 },
-    { name: "11", uv: 4, pv: 0, amt: 0 },
-    { name: "12", uv: 6, pv: 0, amt: 0 },
-    { name: "13", uv: 7, pv: 0, amt: 0 },
-    { name: "14", uv: 9, pv: 0, amt: 0 },
-  ];
+  const [diaryEntries, setDiaryEntries] = useState(null);
+  const [isLoading, setIsLoaded] = useState(false);
+  const userID = sessionStorage.getItem("userID");
 
+  useEffect(() => {
+    async function showLastDiaryEntries() {
+      const newDiaryEntries = await fetchLastDiaryEntries(userID);
+      setDiaryEntries(newDiaryEntries);
+      setIsLoaded(true);
+    }
+    showLastDiaryEntries();
+  }, [userID]);
+
+  if (!isLoading) {
+    return <Loading />;
+  } else if (diaryEntries.length !== 14) {
+    return (
+      <Error
+        message="Du musst erst 14 Tagebucheinträge haben, bevor diese Funktion
+      freigeschaltet wird."
+      />
+    );
+  }
+  const data = [
+    { name: "1", uv: diaryEntries[0].mood, pv: 0, amt: 0 },
+    { name: "2", uv: diaryEntries[1].mood, pv: 0, amt: 0 },
+    { name: "3", uv: diaryEntries[2].mood, pv: 0, amt: 0 },
+    { name: "4", uv: diaryEntries[3].mood, pv: 0, amt: 0 },
+    { name: "5", uv: diaryEntries[4].mood, pv: 0, amt: 0 },
+    { name: "6", uv: diaryEntries[5].mood, pv: 0, amt: 0 },
+    { name: "7", uv: diaryEntries[6].mood, pv: 0, amt: 0 },
+    { name: "8", uv: diaryEntries[7].mood, pv: 0, amt: 0 },
+    { name: "9", uv: diaryEntries[8].mood, pv: 0, amt: 0 },
+    { name: "10", uv: diaryEntries[9].mood, pv: 0, amt: 0 },
+    { name: "11", uv: diaryEntries[10].mood, pv: 0, amt: 0 },
+    { name: "12", uv: diaryEntries[11].mood, pv: 0, amt: 0 },
+    { name: "13", uv: diaryEntries[12].mood, pv: 0, amt: 0 },
+    { name: "14", uv: diaryEntries[13].mood, pv: 0, amt: 0 },
+  ];
   return (
     <BasicPageLayout
       childrenheadsection={<></>}
