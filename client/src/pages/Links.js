a21537db8e469726a0de3609b6dce90cb934985c
slawoe <info@slawomirernst.de>
2020-10-03 20:14:55 +0200

    Add relevant links to Link page

--- client/src/pages/Links.js
+++ client/src/pages/Links.js
@@ -1,6 +1,11 @@
 import React from "react";
 import BasicPageLayout from "../components/BasicPageLayout";
 import StyledTextContainer from "../components/StyledTextContainer";
+import styled from "@emotion/styled";
+
+const LinksHeadline = styled.h2`
+  color: var(--text-color-attention);
+`;
 
 function Links() {
   return (
@@ -11,83 +16,199 @@ function Links() {
             <h1>Nützliche Anlaufstellen</h1>
           </StyledTextContainer>
           <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
+            <LinksHeadline>Nummer gegen Kummer</LinksHeadline>
             <p>
+              <span>
+                Nummer gegen Kummer e.V.
+                <br />
+                Hofkamp 108
+                <br />
+                42103 Wuppertal
+              </span>
+              <span>montags-samstags von 14-20 Uhr</span>
               <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
+                Homepage
+              </a>
+              <a href="tel:116111">Kindertelefon</a>
+              <a href="tel:08001110550">Elterntelefon</a>
+              <a href="mailto:info@nummergegenkummer.de">Mail</a>
+            </p>
+          </StyledTextContainer>
+          <StyledTextContainer>
+            <LinksHeadline>Psychenet</LinksHeadline>
+            <p>
+              <span>
+                Universitätsklinikum Hamburg-Eppendorf (UKE)
+                <br />
+                Martinistraße 52
+                <br />
+                20246 Hamburg
+              </span>
+              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
+                Homepage
+              </a>
+              <a href="tel:+494074100">Telefon</a>
+              <a href="mailto:info@uke.de">Mail</a>
+            </p>
+          </StyledTextContainer>
+          <StyledTextContainer>
+            <LinksHeadline>Neurologen und Psychiater im Netz</LinksHeadline>
+            <p>
+              <span>
+                Monks - Ärzte im Netz GmbH
+                <br />
+                Tegernseer Landstraße 138
+                <br />
+                81539 München
+              </span>
+              <a href="https://www.neurologen-und-psychiater-im-netz.org/kinder-jugend-psychiatrie/uebersicht/">
+                Homepage
+              </a>
+              <a href="tel:+498964248212">Telefon</a>
+            </p>
+          </StyledTextContainer>
+          <StyledTextContainer>
+            <LinksHeadline>Telefonseelsorge</LinksHeadline>
+            <p>
+              <a href="https://www.telefonseelsorge.de/">Homepage</a>
+              <a href="tel:08001110111">Telefon</a>
+              <a href="https://www.telefonseelsorge.de/vor-ort/">
+                Vor-Ort-Beratung
               </a>
             </p>
           </StyledTextContainer>
           <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
+            <LinksHeadline>Kassenärztliche Bundesvereinigung</LinksHeadline>
             <p>
-              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
+              <span>
+                Kassenärztliche Bundesvereinigung KdöR
+                <br />
+                Herbert-Lewin-Platz 2, 10623 Berlin
+                <br />
+                Postfach 12 02 64, 10592 Berlin
+              </span>
+              <a href="https://www.kbv.de/html/arztsuche.php">Homepage</a>
+              <a href="tel:116117">Telefonischer Patientenservice</a>
+              <a href="mailto:info@kbv.de">Mail</a>
+            </p>
+          </StyledTextContainer>
+          <StyledTextContainer>
+            <LinksHeadline>Weisse Liste</LinksHeadline>
+            <p>
+              <span>
+                Weisse Liste gemeinnützige GmbH
+                <br />
+                Sitz der Gesellschaft:
+                <br />
+                Carl-Bertelsmann-Straße 256
+                <br />
+                33311 Gütersloh
+              </span>
+              <a href="https://www.weisse-liste.de/de/arzt/arztsuche/">
+                Homepage
+              </a>
+              <a href="tel:+4930275788300">Telefon</a>
+              <a href="https://www.weisse-liste.de/de/informationen/kontakt-allgemein/">
+                Kontaktformular
               </a>
             </p>
           </StyledTextContainer>
           <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
+            <LinksHeadline>Familienportal des Bundesministeriums</LinksHeadline>
             <p>
-              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
+              <span>
+                Bundesministerium für Familie, Senioren, Frauen und Jugend
+                <br />
+                Glinkastraße 24
+                <br />
+                10117 Berlin
+              </span>
+              <a href="https://familienportal.de/">Homepage</a>
+              <a href="tel:03020179130">Service-Telefon</a>
+              <a href="https://familienportal.de/familienportal/meta/kontakt">
+                Kontaktformular
               </a>
             </p>
           </StyledTextContainer>
           <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
+            <LinksHeadline>
+              Bundeskonferenz für Erziehungsberatung
+            </LinksHeadline>
             <p>
-              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
+              <span>
+                Bundeskonferenz für Erziehungsberatung e.V.
+                <br />
+                Herrnstraße 53
+                <br />
+                D-90763 Fürth
+              </span>
+              <a href="https://www.bke.de/virtual/ratsuchende/beratungsstellen.html?SID=114-7D0-704-C17">
+                Homepage
               </a>
+              <a href="tel:+49911977140">Telefon</a>
+              <a href="mailto:bke@bke.de">Mail</a>
             </p>
           </StyledTextContainer>
           <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
+            <LinksHeadline>DAJEB</LinksHeadline>
             <p>
-              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
-              </a>
+              <span>
+                Deutsche Arbeitsgemeinschaft für Jugend- und Eheberatung e.V.
+                (DAJEB)
+                <br />
+                Neumarkter Str. 84 c <br /> 81673 München
+              </span>
+              <a href="https://www.dajeb.de/">Homepage</a>
+              <a href="tel:+49894361091">Telefon</a>
+              <a href="mailto:info@dajeb.de">Mail</a>
             </p>
           </StyledTextContainer>
           <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
+            <LinksHeadline>Caritas</LinksHeadline>
             <p>
-              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
+              <span>
+                Deutscher Caritasverband e. V. <br />
+                Karlstraße 40 <br />
+                79104 Freiburg
+              </span>
+              <a href="https://www.caritas.de/hilfeundberatung/onlineberatung/kinder-jugendliche/start">
+                Homepage
               </a>
+              <a href="tel:+497612000">Telefon</a>
+              <a href="mailto:info@caritas.de">Mail</a>
             </p>
           </StyledTextContainer>
           <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
+            <LinksHeadline>Bundes Psychotherapeuten Kammer</LinksHeadline>
             <p>
-              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
+              <span>
+                Bundespsychotherapeutenkammer (BPtK)
+                <br />
+                Arbeitsgemeinschaft der Landespsychotherapeutenkammern
+                <br />
+                Klosterstraße 64
+                <br />
+                10179 Berlin
+              </span>
+              <a href="https://www.bptk.de//service/therapeutensuche/">
+                Homepage
               </a>
+              <a href="tel:0302787850">Telefon</a>
+              <a href="mailto:info@bptk.de">Mail</a>
             </p>
           </StyledTextContainer>
           <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
+            <LinksHeadline>Psychotherapie Informationsdienst</LinksHeadline>
             <p>
-              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
-              </a>
-            </p>
-          </StyledTextContainer>
-          <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
-            <p>
-              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
-              </a>
-            </p>
-          </StyledTextContainer>
-          <StyledTextContainer>
-            <h2>Nummer gegen Kummer</h2>
-            <p>
-              <a href="https://www.nummergegenkummer.de/kinder-und-jugendtelefon.html">
-                Link
-              </a>
+              <span>
+                Psychotherapie- Informationsdienst (PID)
+                <br />
+                Am Köllnischen Park 2<br />
+                10179 Berlin
+              </span>
+              <a href="https://www.psychotherapiesuche.de/">Homepage</a>
+              <a href="tel:030209166330">Telefon</a>
+              <a href="mailto:pid@psychologenakademie.de">Mail</a>
             </p>
           </StyledTextContainer>
         </>
